8ce36ac090d5c9198702eead79dcaed5f7697a85
Darius Schefer <darius1702@gmail.com>
2023-06-02 06:04:15 +0200

    remove evil and counsel, use icomplete-mode instead

--- init.el
+++ init.el
@@ -5,6 +5,9 @@
 (scroll-bar-mode 0)
 (tooltip-mode 0)
 (blink-cursor-mode 0)
+(hl-line-mode t)
+(icomplete-mode t)
+(icomplete-vertical-mode t)
 
 ;; No annoying undo and backup files everywhere
 (make-directory (expand-file-name "tmp/auto-saves/" user-emacs-directory) t)
@@ -31,9 +34,6 @@
 ;; Probably something good
 (setq frame-resize-pixelwise t)
 
-;; Yes
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
 (add-to-list 'write-file-functions 'delete-trailing-whitespace)
 
 ;; Package setup
@@ -51,46 +51,6 @@
 (use-package diminish)
 
 (use-package gruber-darker-theme)
-
-(defun ds/evil-hook ()
-  (dolist (mode '(custom-mode
-		  eshell-mode
-		  git-rebase-mode
-		  term-mode))
-    (add-to-list 'evil-emacs-state-modes mode)))
-
-(use-package evil
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump t)
-  (evil-mode 1)
-  :hook (evil-mode . ds/evil-hook)
-  :after counsel
-  :config
-  (evil-set-leader 'normal (kbd "SPC"))
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-  (evil-define-key 'normal 'global (kbd "<leader>SPC") 'find-file)
-  (evil-define-key 'normal 'global (kbd "<leader>fb") 'ivy-switch-buffer)
-  (evil-define-key 'normal 'global (kbd "<leader>/") 'counsel-grep-or-swiper)
-  (evil-define-key 'normal 'global (kbd "<leader>hp") 'git-gutter:popup-hunk))
-
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
-
-(use-package counsel
-  :diminish ivy-mode
-  :bind (("M-x" . counsel-M-x))
-  :config
-  (ivy-mode 1))
-
-(use-package ivy-rich
-  :init
-  (ivy-rich-mode 1))
 
 (use-package undo-tree
   :diminish
